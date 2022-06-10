cb42099b53489dd1d652d2f38f12d765f771d2e8
Yeonhyuk <brankein13@gm.gist.ac.kr>
2022-06-10 11:17:45 +0900

    Initial commit

---
+++ src/main/java/com/yhkim/springdynamicssl/SpringDynamicSslApplication.java
@@ -0,0 +1,13 @@
+package com.yhkim.springdynamicssl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDynamicSslApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDynamicSslApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yhkim/springdynamicssl/SpringDynamicSslApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yhkim.springdynamicssl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDynamicSslApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
